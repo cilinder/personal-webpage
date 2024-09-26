d58c074d096140779b14d4741e91382621779702
cilinder <jure.taslak@gmail.com>
2024-09-26 10:49:02 +0200

    make custom domain the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cilinder.github.io',
-    base: 'personal-webpage',
+    site: 'https://taslak.si'
 });
